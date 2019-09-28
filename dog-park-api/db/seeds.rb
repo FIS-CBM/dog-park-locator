0a20d2e8d50c1044f492e826b742923bdc225ea0
sachinm78 <sachinm78@gmail.com>
2019-09-28 11:42:09 -0500

    seeded db

--- dog-park-api/db/seeds.rb
+++ dog-park-api/db/seeds.rb
@@ -8,6 +8,6 @@
 
 #  dogParks = DogPark.create(name: "Park1", city: "Chicago", state: "Illinois", zip_code: 60007, address: "123 Dog Park Lane", website: "www.ninjadogparks.com", info: "I don't know a damn thing about this dog park.", fenced: true, hours: "4am-7pm")
 
-# 25.times do
-#     Dogpark.create(name: Faker::Creature::Dog.meme_phrase, city: Faker::Address.city, state: Faker::Address.state, zip_code: Faker::Address.zip, address: Faker::Address.street_address, website: Faker::Internet.domain_name, info: Faker::Lorem.sentence, fenced: Faker::Boolean.boolean, hours: "9am to 9pm")
-# end
+25.times do
+    DogPark.create(name: Faker::Creature::Dog.meme_phrase, city: Faker::Address.city, state: Faker::Address.state, zip_code: Faker::Address.zip, address: Faker::Address.street_address, website: Faker::Internet.domain_name, info: Faker::Lorem.sentence, fenced: Faker::Boolean.boolean, hours: "9am to 9pm")
+end
