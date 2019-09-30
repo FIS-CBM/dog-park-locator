4e7d1bee49a6a8179681f0d135841c59a4b0377e
someotherkyle <kyle@someotherkyle.com>
2019-09-30 17:11:42 -0400

    got form showing and logging all but fenced checkbox

--- dog-park-frontend/src/containers/Parks.js
+++ dog-park-frontend/src/containers/Parks.js
@@ -5,7 +5,18 @@ export default class Parks extends Component {
     super()
     this.state = {
       parks: [],
-      showAdd: false
+      showAdd: false,
+      new: {
+        name: '',
+        street: '',
+        city: '',
+        state: '',
+        zip_code: '',
+        hours: '',
+        info: '',
+        website: '',
+        fenced: false
+      }
     }
 
   }
@@ -31,17 +42,57 @@ export default class Parks extends Component {
     })
   }
 
+  handleFieldChange = e => {
+    e.preventDefault()
+    this.setState({
+      ...this.state,
+      new: {
+        ...this.state.new,
+        [e.target.id]: e.target.value
+      }
+    })
+  }
+
+  handleFenceChange = e => {
+    e.preventDefault()
+    this.setState({
+      ...this.state,
+      new: {
+        ...this.state.new,
+        fenced: !this.state.new.fenced
+      }
+    })
+
+  }
+
   render(){
     return(
       <div className='parks-container'>
         <div className='row'>
-          <div className='col-sm-2'>
+          <div className='col-xs-4 col-sm-2'>
             <button onClick={e => this.handleShowAdd(e)}>Add Park</button>
           </div>
         </div>
+      {this.state.showAdd ? 
+        <div className='col-med-8 add-park-form'>
+          <form>
+            <p>Name: <input id='name' onChange={e => this.handleFieldChange(e)} /></p>
+            <p>Street: <input id='address' onChange={e => this.handleFieldChange(e)} /></p>
+            <p>City: <input id='city' onChange={e => this.handleFieldChange(e)} /></p>
+            <p>State: <input id='state' onChange={e => this.handleFieldChange(e)} /></p>
+            <p>Zip Code: <input id='zip_code' onChange={e => this.handleFieldChange(e)} /></p>
+            <p>Hours: <input id='hours' onChange={e => this.handleFieldChange(e)} /></p>
+            <p>Info: <input id='info' onChange={e => this.handleFieldChange(e)} /></p>
+            <p>Website: <input id='website' onChange={e => this.handleFieldChange(e)} /></p>
+            <p>Fenced: <input type='checkbox' id='fenced' onClick={e => this.handleFieldChange(e)} /></p>
+            <button type='submit'>Submit</button>
+          </form>
+
+        </div> :
         <div className='row'>
           <div className='col-md-8'>
             <table>
+              <tbody>
               <tr>
                 <th>Name</th>
                 <th>Hours</th>
@@ -50,7 +101,7 @@ export default class Parks extends Component {
               </tr>
                 {this.state.parks.map(park => {
                   return(
-                    <tr>
+                    <tr key={park.id}>
                       <td>{park.name}</td>
                       <td>{park.hours}</td>
                       <td>{park.address}<br />{park.city}, {park.state}</td>
@@ -58,9 +109,10 @@ export default class Parks extends Component {
                     </tr>
                   )
                 })}
+              </tbody>
             </table>
           </div>
-        </div>
+        </div>}
       </div>
     )
   }
