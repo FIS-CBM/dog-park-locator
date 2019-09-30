462eb571ed92166995e2cac5c83c9acfb0acd36c
someotherkyle <kyle@someotherkyle.com>
2019-09-30 14:54:40 -0400

    added react-router-dom dep + removed app.css

--- dog-park-frontend/src/App.js
+++ dog-park-frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+    <div className="App container-fluid">
+      <div className='navbar-wrapper'>
+        <Nav />
+      </div>
     </div>
+
+    </Router>
   );
 }
 
