1addd892f7c0b535518010daf304911f23ea44cc
someotherkyle <kyle@someotherkyle.com>
2019-09-30 16:17:49 -0400

    bare minimum landing page

---
+++ dog-park-frontend/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Landing extends Component {
+
+
+  render(){
+    return(
+      <div>
+        <h1>Where the pups!?</h1>
+      </div>
+    )
+  }
+}
