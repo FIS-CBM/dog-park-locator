bca04185a31453f49c9ab58acbbc4d6c4b61d6ce
someotherkyle <kyle@someotherkyle.com>
2019-09-30 16:17:10 -0400

    added nav landing and parks to app

--- dog-park-frontend/src/App.js
+++ dog-park-frontend/src/App.js
@@ -3,16 +3,18 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom'
+import Nav from './components/Nav'
+import Landing from './components/Landing'
+import Parks from './containers/Parks'
 
 function App() {
   return (
     <Router>
-    <div className="App container-fluid">
-      <div className='navbar-wrapper'>
+      <div className="App container-fluid">
         <Nav />
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/parks' component={Parks} />
       </div>
-    </div>
-
     </Router>
   );
 }
