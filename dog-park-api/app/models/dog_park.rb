75e22b915c832baeb0d4aee175f14282746d01f9
Jon <github email address>
2019-10-01 10:54:06 -0400

    Added validations for name and address uniqueness

--- dog-park-api/app/models/dog_park.rb
+++ dog-park-api/app/models/dog_park.rb
@@ -1,5 +1,7 @@
 class DogPark < ApplicationRecord
 
 	validates :name, :city, :state, :zip_code, :address, :website, :info, :fenced, :hours, presence: true
-	
+
+	validates :name, :address, uniqueness: true
+
 end
