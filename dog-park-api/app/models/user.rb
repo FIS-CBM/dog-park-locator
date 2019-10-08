8bc598d300c3e5c67f34a2ec74166914dcf87127
BMWeygant <bobbydigital108@gmail.com>
2019-10-08 14:17:03 -0400

    Added devise gem, created devise users model & migration.

--- dog-park-api/app/models/user.rb
+++ dog-park-api/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    :has_secure_password
-    validates :name, :email, :password, presence: true
-    validates :email, uniqueness: true
-    validates :password, confirmation: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
